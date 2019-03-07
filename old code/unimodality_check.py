9bfb9d36e24188e5d1afdc48bb264bedec9fb62a
Unknown <hej.mellor@gmail.com>
2019-03-07 22:14:25 +0000

    Spotted error in unimodality check, still empirically unimodal tho

--- old code/unimodality_check.py
+++ old code/unimodality_check.py
@@ -28,47 +28,23 @@ def zehan_iou(input, target, size):
         I = indices.sum(0)
         sigma -= I.float() / U.float()
         sigma[1:] += theta_hat[U[1:] - n_gt - 1]
-        if (theta > 0).sum() > 1 and (indices.float().t() * theta[mask_gt]).sum(1).unique().numel() > 1:
-            print(theta_hat[U[1:] - n_gt - 1] - (I.float() / U.float())[1:])
-  #          print(np.gradient((theta_hat[U[1:] - n_gt - 1] - (I.float() / U.float())[1:]).cpu().detach().numpy()))
-#            print(min(theta_tilde.abs()), 1./n_gt.float())
-            #print(((indices.float().t() * theta[mask_gt]).sum(1)[0:-1] - (indices.float().t() * theta[mask_gt]).sum(1)[1:]))
-            print((indices.float().t() * theta[mask_gt]).sum(1), I, U.numel())
-            diff_thet = ((indices.float().t() * theta[mask_gt]).sum(1)[1:] - (indices.float().t() * theta[mask_gt]).sum(1)[0:-1])
-            print(diff_thet, 1/U.float())
-            diff_iou  = ((I.float() / U.float())[1:] - (I.float() / U.float())[0:-1])
-            print("thet diff:", diff_thet,"iou diff", diff_iou)
-            print((diff_thet-diff_iou).abs(), "\n")
-            #print(((I.float() / U.float())[0:-1] - (I.float() / U.float())[1:]))
-            print("\nNumber of sign changes:{}".format(((np.diff(np.sign(np.gradient(((indices.float().t() * theta[mask_gt]).sum(1)).cpu().detach().numpy()))) != 0) * 1).sum()))
-           # print(np.gradient((I.float() / U.float()).cpu().detach().numpy()))
-     #       print(theta_tilde.abs() < 1./U[1:].float().sum())
-          #  assert (theta_tilde.abs() < 1./U[1:].float()).sum() == 0, "\nNumber of sign changes:{}".format(((np.diff(np.sign(np.gradient((I.float() / U.float()).cpu().detach().numpy()))) != 0) * 1).sum())
-     #       print("New sample\n")
-     #       print("theta", theta)
-     #       print("theta[gt] > 1/U:", (indices.float().t() * theta[mask_gt]).sum(1))
-     #       print(I.float())
-     #       print(np.gradient((I.float() / U.float()).cpu().detach().numpy()))
-     #       print((np.diff(np.sign(np.gradient((I.float() / U.float()).cpu().detach().numpy()))) != 0) * 1)
-     #       print("unique in theta[gt] > 1/U:", (indices.float().t() * theta[mask_gt]).sum(1).unique())
-     #       print("IoU:", I.float() / U.float())
-     #       print("theta_hat:", theta_hat[U[1:] - n_gt - 1], "\n")
+        # Check that sigma is unimodal
+        grads = np.diff(sigma.cpu().detach().numpy())
+        signs = (np.diff(np.sign(grads[grads != 0])) != 0) * 1
+        assert signs.sum() <= 1, 'sparse sigma is not unimodal'
         loss[i] = sigma.max()
         loss[i] += 1 - theta[mask_gt].sum()
-    return loss.mean(), sigma
+    return loss.mean()
 
 
-n_pixels = 30
+n_pixels = 2000
 c_classes = 20
 size = None
 torch.manual_seed(0)
 
-for _ in range(1000):
+for _ in range(10):
     target = torch.randint(0, 2, (n_pixels,)) * uniform(0, c_classes)
     if target.sum() == 0:
         continue
     input = torch.zeros(n_pixels, c_classes).uniform_(-10, 10)
-    _, sigma = zehan_iou(input, target, size)
-    grads = np.gradient(sigma.cpu().detach().numpy())
-    signs = (np.diff(np.sign(grads)) != 0) * 1
-    assert signs.sum() <= 1, "Number of sign changes: {}".format(signs.sum())
+    _ = zehan_iou(input, target, size)
