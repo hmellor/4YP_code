bc8c477bc9b3b8945f575137ba8cc7c1837ff131
HMellor <hej.mellor@gmail.com>
2019-03-29 19:19:48 +0000

    Script to reload all visdom data in event of server restart

---
+++ reload_visdom.py
@@ -0,0 +1,13 @@
+import logger
+import os
+
+
+for root, dirs, files in os.walk("./runs"):
+    for file in files:
+        if file.endswith('.json') and 'archive' not in root:
+            file_path = os.path.join(root, file)
+            print('Loading: ', file_path)
+            xp = logger.Experiment('dummy_name')
+            xp.from_json(file_path)
+            xp.to_visdom(visdom_opts={'server': 'http://localhost', 'port': 8098})
+            print('Success!')
