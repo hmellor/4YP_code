42e21911381661eaca9aa6047bb6c4a2d497677d
HMellor <hej.mellor@gmail.com>
2019-04-01 14:09:53 +0100

    Logger from_json() bugfix

--- reload_visdom.py
+++ reload_visdom.py
@@ -9,5 +9,8 @@ for root, dirs, files in os.walk("./runs"):
             print('Loading: ', file_path)
             xp = logger.Experiment('dummy_name')
             xp.from_json(file_path)
+            for opts in xp.visdom_win_opts.values():
+                if 'legend' in opts:
+                    opts.pop('legend')
             xp.to_visdom(visdom_opts={'server': 'http://localhost', 'port': 8098})
             print('Success!')
